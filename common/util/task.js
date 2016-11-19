d7170ebe397bcf5dc243f00760ed723065d569e6
Gary <crazycat836@gmail.com>
2016-11-19 17:44:33 +0800

    add util

---
+++ common/util/task.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const CronJob = require('cron').CronJob;
+const CONFIG = require('../../config/config');
+const dcardAPI = require('../api/api');
+const Spider = require('./spider');
+const DateCalc = require('./date');
+
+
+// ============== BAE node-log ==============
+if(CONFIG.log.openBae){
+    const logger = console;
+}else {
+    const logger = require('log4js').getLogger('cheese');
+}
+
+const Task = {
+    fire: function(){
+        this.hourly();
+        this.daily();
+        this.weekly();
+    },
+    // 07:00 - 22:00 每1个小时爬取一次lastest
+    hourly: function(){
+        new CronJob('00 00 7-22/1 * * *', function(){
+            Spider.latest();
+        }, function(){
+            logger.info('hourly cron-job over')
+        }, true, 'Asia/Shanghai');
+    },
+    // 每天23:30 爬取当天的数据
+    daily: function(){
+        new CronJob('00 30 23 * * *', function(){
+            var date = new DateCalc().after();
+            Spider.day(date);
+        }, function(){
+            logger.info('daily cron-job over @date:' + new Date())
+        }, true, 'Asia/Shanghai');
+    },
+    
+    // 每周三、日 00:30 更新前7天的评论点赞数 
+    // 从start到end前一天 共7天
+    weekly: function(){
+        new CronJob('00 30 00 * * 0,3', function(){
+            var start = new DateCalc().before(),
+                end = new DateCalc().before(8);
+            Spider.updateCmtCount(start, end);
+        }, function(){
+            logger.info('weekly cron-job over ')
+        }, true, 'Asia/Shanghai');
+    }
+}
+
+module.exports = Task;
