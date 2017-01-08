b4ea3b0c46247c83787e138b914d011d2302bd33
Gary <crazycat836@gmail.com>
2017-01-08 13:09:51 +0800

    update promise libraries bluebird

--- common/db/connect.js
+++ common/db/connect.js
@@ -4,6 +4,10 @@ const config = require('../../config/config').mongo;
 
 const dbUrl = config.url();
 const dbOption = config.mongoOptions;
+
+// Use bluebird
+mongoose.Promise = require('bluebird');
+
 mongoose.connect(dbUrl, dbOption);
 
 exports.mongoose = mongoose;
