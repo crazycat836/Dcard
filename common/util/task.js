03eca12345f3ee64d1e126a853e3944cfa646bfc
Gary <crazycat836@gmail.com>
2017-01-10 16:38:17 +0800

    Update task

--- common/util/task.js
+++ common/util/task.js
@@ -1,58 +1,61 @@
 "use strict";
 
 const CronJob = require('cron').CronJob;
-const CONFIG = require('../../config/config');
-const dcardAPI = require('../api/api');
 const Spider = require('./spider');
 const DateCalc = require('./date');
-
+const d = new DateCalc();
+const logger = require('log4js').getLogger('access');
 /*
 Cron Ranges
 
-Seconds: 0-59
-Minutes: 0-59
-Hours: 0-23
-Day of Month: 1-31
-Months: 0-11
-Day of Week: 0-6
-
+ # ┌────────────── second (optional)
+ # │ ┌──────────── minute
+ # │ │ ┌────────── hour
+ # │ │ │ ┌──────── day of month
+ # │ │ │ │ ┌────── month
+ # │ │ │ │ │ ┌──── day of week
+ # │ │ │ │ │ │
+ # │ │ │ │ │ │
+ # * * * * * *
+ 
  */
 
 const Task = {
-    fire: function(){
+    fire: function() {
         this.hourly();
         this.daily();
         this.weekly();
     },
-    // 07:00 - 22:00 每1个小时爬取一次lastest
-    hourly: function(){
-        new CronJob('00 00 7-22/1 * * *', function(){
+    // 07:00 - 22:00 每小時爬取一次最新熱門文章
+    hourly: function() {
+        new CronJob('00 00 7-22/1 * * *', function() {
             Spider.latest();
-        }, function(){
-            logger.info('hourly cron-job over')
+        }, function() {
+            logger.info('hourly cron-job over @time:' + d.now())
         }, true, 'Asia/Taipei');
     },
-    // 每天23:30 爬取当天的数据
-    daily: function(){
-        new CronJob('00 30 23 * * *', function(){
-            var date = new DateCalc().after();
-            Spider.day(date);
-        }, function(){
-            logger.info('daily cron-job over @date:' + new Date())
+    // 每天23:30 抓取當天所有文章
+    daily: function() {
+        new CronJob('00 30 23 * * *', function() {
+            Spider.day();
+        },function(){
+            Spider.dayRefresh(d.today());
+        }, function() {
+            logger.info('daily cron-job over @date:' + d.today());
         }, true, 'Asia/Taipei');
     },
-    
-    // 每周三、日 00:30 更新前7天的评论点赞数 
-    // 从start到end前一天 共7天
-    weekly: function(){
-        new CronJob('00 30 00 * * 0,3', function(){
-            var start = new DateCalc().before(),
-                end = new DateCalc().before(8);
+    // 每周三、日 00:30 更新七天前評論數
+    // 從 Start 到 End 前一天
+    weekly: function() {
+        new CronJob('00 30 00 * * 0,3', function() {
+            const start = d.before(),
+                end = d.before(8);
             Spider.updateCmtCount(start, end);
-        }, function(){
+            Spider.updateForumsInfo();
+        }, function() {
             logger.info('weekly cron-job over ')
         }, true, 'Asia/Taipei');
     }
-}
+};
 
 module.exports = Task;
