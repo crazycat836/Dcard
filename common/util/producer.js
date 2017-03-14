755373781c16943ef3e280217b6dc6e767c60d2d
Gary <crazycat836@gmail.com>
2017-03-14 16:31:15 +0800

    add node kafka producer

---
+++ common/util/producer.js
@@ -0,0 +1,18 @@
+const Kafka = require('no-kafka');
+const producer = new Kafka.Producer();
+
+const tagProducer = {
+    Stream: function(d) {
+        return producer.init().then(function() {
+            return producer.send({
+                topic: 'test',
+                partition: 0,
+                message: {
+                    value: d
+                }
+            });
+        });
+    }
+};
+
+module.exports = tagProducer;
