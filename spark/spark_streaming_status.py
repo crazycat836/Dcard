eecd16b1f0f7c3a1fabde2e3e13b38debf03b188
Gary <crazycat836@gmail.com>
2017-03-14 16:31:49 +0800

    change python kafka library

--- spark/spark_streaming_status.py
+++ spark/spark_streaming_status.py
@@ -1,30 +1,31 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from pykafka import KafkaClient
+from kafka import KafkaProducer
 import json
 import sys
 import pprint
-reload(sys)
-sys.setdefaultencoding('utf8')
 
-#def pushOrderStatusInKafka(tags_counts):
-#   client = KafkaClient(hosts="localhost:9092")
-#   topic = client.topics['order-one-min-data']
-#    for tags_count in tags_counts:
-#	    with topic.get_producer() as producer:
-#		    producer.produce(json.dumps(tags_count))
+
+
+def pushTagCountInKafka(tags_counts):
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    for tag in tags_counts:
+        tag_string = json.dumps(tag)
+        producer.send('one-min-data', tag_string.encode('utf-8'))
 
 zkQuorum, topic = sys.argv[1:]
 sc = SparkContext(appName="DcardTagCount")
-ssc = StreamingContext(sc, 45)
+sc.setLogLevel("ERROR")
+ssc = StreamingContext(sc, 120)
 kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
 lines = kvs.map(lambda x: x[1])
 lines.pprint()
-tags_count = lines.map(lambda line: line.split(",")) \
-              .map(lambda tag_status: (tag_status, 1)) \
+tags_count = lines.flatMap(lambda line: line.split(","))\
+             .map(lambda tag_status: (tag_status, 1)) \
               .reduceByKey(lambda a, b: a+b)
 tags_count.pprint()
-#tags_count.foreachRDD(lambda rdd: rdd.foreachPartition(pushOrderStatusInKafka))
+
+tags_count.foreachRDD(lambda rdd: rdd.foreachPartition(pushTagCountInKafka))
 ssc.start()
 ssc.awaitTermination()
